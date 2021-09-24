fd1e14a9e928f019d066a993325b6b49cf5a2ef8
Alper KILIÇKAYA <alperkilickaya@gmail.com>
2021-09-24 18:26:08 +0300

    background added

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -9,18 +9,31 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
+import { makeStyles } from '@material-ui/core';
+
 
     const number = [1,2,3,4,5,6,7]
 
 
-    export default function RecipeReviewCard() {
+    const useStyles = makeStyles({
+        blogContainer:{
+            backgroundImage: `url(${"https://picsum.photos/1200/900"})`,
+            backgroundSize:"100% 100%",
+           
+            
+        }
+    })
 
+
+export default function BlogCard() {
+
+    const classes = useStyles();
 
   return (
-    <Grid container spacing={0}>
+    <Grid container spacing={0} className={classes.blogContainer}>
         {number.map((el,index) => {
             return (
-            <Grid item xs={12} sm={4} >
+            <Grid item xs={12} sm={6} md={4} >
                 <div style={{padding:"40px"}}>
                 <Card key={index} >
                     <CardHeader
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import AppBar from '../components/AppBar';
 import Blogcard from '../components/BlogCard';
 
 function Main() {
